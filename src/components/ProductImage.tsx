1456056246b5d976fb4f7c1c0eaab5b60229e9c5
Dario <dramirez@balanzashook.com.ar>
2024-04-29 14:37:08 -0300

    Version 1.0.0-rc

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
+
 import { ProductContext } from "./ProductCard";
 
 import styles from "../styles/styles.module.css";
